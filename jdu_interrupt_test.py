d722d3750f246c6fc791de5780dab6b7f8a9119b
Agngus_home_ubuntu <lwj4578@qq.com>
2023-05-14 10:17:42 -0400

    successful interrupt

--- jdu_interrupt_test.py
+++ jdu_interrupt_test.py
@@ -5,14 +5,17 @@ import time
 
 if __name__ == '__main__':
     os.chdir('/usr/local/gn')
-    base_url = "/tmp/JABRA_FILI_OTA_V1.1.8.zip"
+    base_url = "/home/angus/Desktop/fw/Jabra_Link_380-v1.16.0-l380a-vector.zip"
     subprocess.Popen(['./jfwu', base_url])
     usber=UsbBoxDriver_ubuntu()
-# 判断/tmp/jfwu_log/下的jfwu.log中的文本是否有50%这个关键字，如果没有，就每隔一秒钟检查一次，如果有，则执行disconnect_usb_box()这个函数，断开usb box的连接。
-    with open("/tmp/jfwu_log/jfwu.log") as f:
-        while "50%" in f.read()==False:
-            time.sleep(1)
-            print('Keyword not found!The zip download still continue.')
+# 判断/tmp/jfwu_log/下的jfwu.log中的文本是否有50%这个关键字，如果没有，就每隔一秒钟检查一次，如果有，则执行disconnect_usb_box()这个函数，断开usb box的连接。/
+    while not os.path.exists('/tmp/jfwu_log/jfwu.log'):
+        time.sleep(1)
+
+    while "50%" not in open('/tmp/jfwu_log/jfwu.log').read():
+        time.sleep(1)
+        print('Keyword not found!The zip download still continue')
+
     print('Keyword found!')
     usber.disconnect_usb_box()
     time.sleep(10)
