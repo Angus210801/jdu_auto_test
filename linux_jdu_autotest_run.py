11e2cddec8d1bfc37905aedccf159775c0cbddf9
AngusLin <anlin@jabra.com>
2023-06-21 13:40:39 +0800

    Update: add the wait time before settings compare.

--- linux_jdu_autotest_run.py
+++ linux_jdu_autotest_run.py
@@ -145,7 +145,7 @@ def run_testcase_update_settings_for_new_device(prepare_case_id, case_id, server
         # 4. Compare the settings after the test case
         f.write(f"\nstep3:check if the settings changed correctly:\n")
         f.flush()
-
+        time.sleep(10)
         setting_compare(f)
 
         # 5. Print the test finish info and dividing line to the log file
